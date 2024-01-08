9b525e29467cf9e47b09ec3d336ae35e04a85790
sk3136 <128195933+sk3136@users.noreply.github.com>
2024-01-08 11:23:45 +0900

    Add files via upload

---
+++ s_17.js
@@ -0,0 +1,50 @@
+var Calendar_s_17 = [
+    // moeru
+    [],
+    // moenai
+    ['04','05','06','07','08','09','10','11','12','13','14','15'],
+    // akikan
+    ['04','04','05','05','06','06','07','07','08','08','09','09','10','10','11','11','12','12','13','13','14','14','15','15'],
+    // happou
+    ['04','05','06','07','08','09','10','11','12','13','14','15'],
+    // furununo
+    ['04','05','06','07','08','09','10','11','12','13','14','15'],
+    // sinbun
+    ['04','05','06','07','08','09','10','11','12','13','14','15'],
+    // akibin
+    ['04','05','06','07','08','09','10','11','12','13','14','15']
+];
+
+function moeru_s_17() {
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = now.getMonth() + 1;
+    var month_now = month;
+    var date_now = 1;
+    for (var i = 0; i < month_days_Number(month) + month_days_Number(month + 1); i++) {
+        var dayOfWeek = new Date(year, month_now - 1, date_now).getDay();
+        if (i == month_days_Number(month) + 1) {
+            if (month_now + 1 >= 13) {
+                year++
+                month_now = month_now - 11;
+            } else {
+                month_now++
+            }
+            date_now = 1;
+        }
+        if (month_now == 4 && date_now == 27 || month_now == 4 && date_now == 28) {
+        } else if (dayOfWeek == 1 || dayOfWeek == 3 || dayOfWeek == 5) {
+            if (month_now > 3) {
+                Calendar_s_17[0].push(month_now + '/' + date_now);
+            } else {
+                Calendar_s_17[0].push(Number(month_now + 12) + '/' + date_now);
+            }
+        }
+        date_now++;
+    }
+
+    document.getElementById('title_4').textContent = '雑誌類';
+    document.getElementById('title_5').textContent = '段ボール';
+    document.getElementById('title_6').textContent = '空きびん、発泡スチロール、新聞';
+    document.getElementById('title_7').textContent = '古布';
+}
---
+++ t_13.js
@@ -0,0 +1,45 @@
+var Calendar_t_13 = [
+    // moeru
+    [],
+    // moenai
+    ['04','05','06','07','08','09','10','11','12','13','14','15'],
+    // akikan
+    ['04','04','05','05','06','06','07','07','08','08','09','09','10','10','11','11','12','12','13','13','14','14','15','15'],
+    // happou
+    ['04','05','06','07','08','09','10','11','12','13','14','15'],
+    // furununo
+    ['04','05','06','07','08','09','10','11','12','13','14','15'],
+    // sinbun
+    ['04','05','06','07','08','09','10','11','12','13','14','15'],
+    // akibin
+    ['04','05','06','07','08','09','10','11','12','13','14','15']
+];
+
+function moeru_t_13() {
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = now.getMonth() + 1;
+    var month_now = month;
+    var date_now = 1;
+    for (var i = 0; i < month_days_Number(month) + month_days_Number(month + 1); i++) {
+        var dayOfWeek = new Date(year, month_now - 1, date_now).getDay();
+        if (i == month_days_Number(month) + 1) {
+            if (month_now + 1 >= 13) {
+                year++
+                month_now = month_now - 11;
+            } else {
+                month_now++
+            }
+            date_now = 1;
+        }
+        if (month_now == 4 && date_now == 27 || month_now == 4 && date_now == 28) {
+        } else if (dayOfWeek == 1 || dayOfWeek == 3 || dayOfWeek == 5) {
+            if (month_now > 3) {
+                Calendar_t_13[0].push(month_now + '/' + date_now);
+            } else {
+                Calendar_t_13[0].push(Number(month_now + 12) + '/' + date_now);
+            }
+        }
+        date_now++;
+    }
+}
